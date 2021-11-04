72c9e61d277e7bb1b76ebe194351d18143b57529
ariescat-wy <1275563227@qq.com>
2021-11-04 10:53:43 +0800

    feature(example): 修改Test用例

--- common-hotswap-example/src/main/java/com/ariescat/hotswap/example/instrument/RedefineBean.java
+++ common-hotswap-example/src/main/java/com/ariescat/hotswap/example/instrument/RedefineBean.java
@@ -10,4 +10,19 @@ public class RedefineBean {
         System.out.println("print ori");
 //        System.out.println("print after");
     }
+
+    public static class InnerClass2 {
+        private int j;
+
+        public InnerClass2(int j) {
+            this.j = j;
+        }
+
+        @Override
+        public String toString() {
+            return "InnerClass2{" +
+                    "j=" + j +
+                    '}';
+        }
+    }
 }
--- common-hotswap-example/src/main/java/com/ariescat/hotswap/example/javacode/TestJavaCompiler.java
+++ common-hotswap-example/src/main/java/com/ariescat/hotswap/example/javacode/TestJavaCompiler.java
@@ -3,18 +3,30 @@ package com.ariescat.hotswap.example.javacode;
 import com.ariescat.hotswap.javacode.ScriptClassLoader;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class TestJavaCompiler {
 
-    public static void main(String[] args) {
-        try {
-            ScriptClassLoader classLoader = new ScriptClassLoader(Thread.currentThread().getContextClassLoader());
-            File javaSourceFile = new File(System.getProperty("user.dir") +
-                    "/common-hotswap-example/src/main/java/com/ariescat/hotswap/example/instrument/RedefineBean.java");
-            Class<?> clazz = classLoader.parseClass(javaSourceFile);
-            System.err.println(clazz);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws Exception {
+
+        ScriptClassLoader classLoader = new ScriptClassLoader();
+        File javaSourceFile = new File(System.getProperty("user.dir") +
+                "/common-hotswap-example/src/main/java/com/ariescat/hotswap/example/instrument/RedefineBean.java");
+        Class<?> clazz = classLoader.parseClass(javaSourceFile);
+        System.err.println(clazz);
+
+        Object bean = clazz.newInstance();
+        Method method = clazz.getMethod("print");
+        method.invoke(bean);
+
+        // 这里要用clazz.getClassLoader()，它是InnerLoader加载的
+        Class<?> innerClazz = Class.forName("com.ariescat.hotswap.example.instrument.RedefineBean$InnerClass2", true, clazz.getClassLoader());
+        System.err.println(innerClazz);
+
+        Field[] declaredFields = innerClazz.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            System.err.println(declaredField.getName());
         }
     }
 }
